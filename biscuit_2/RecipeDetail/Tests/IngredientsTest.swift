3df55cb139db32483974647b988ad53e221d04f6
Kelly Bielaski <knbielaski@yahoo.com>
2025-07-23 10:27:58 -0700

    add test cases to recipe detail and ingredient group

---
+++ biscuit_2/RecipeDetail/Tests/IngredientsTest.swift
@@ -0,0 +1,42 @@
+//
+//  IngredientsTest.swift
+//  biscuit_2
+//
+//  Created by kelly on 7/23/25.
+//
+
+import Testing
+
+struct IngredientsTests {
+    // All elements are on the screen
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+    // Empty state shows option to add ingredients
+    
+    // Cannot update if not in editMode
+    
+    // Can update item in ingredient list and ingredient group name
+    
+    // Can delete item in ingredient list and ingredient group
+    
+    // Can add item to ingredient list and new ingredient group
+    
+    // When adding new ingredient, focus is on new item in list
+    
+    // When adding a new ingredient group, focus is on new ingredient group heading
+    
+    // If the last element in ingredient list is empty, don't let user add another ingredient
+    
+    // If the last ingredient group has an empty name, don't let user add another ingredient group
+    
+    // Show “Create name” in ingredient group menu if name is empty and "Rename" if name is not empty
+    
+    // Show error if section name is empty on save
+    
+    // Show error if ingredient group is empty on save
+    
+    // Remove empty ingredients on sav
+    
+}
---
+++ biscuit_2/RecipeDetail/Tests/RecipeDetailTest.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeDetailTest.swift
+//  biscuit_2
+//
+//  Created by kelly on 7/23/25.
+//
+
+import Testing
+
+struct RecipeDetailTests {
+
+    // All elements are on the screen
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+    // Empty state shows option to add stats
+    
+    // Cannot update if in editMode
+    
+    // Title shows error if empty
+    
+    // Can update fields title, description, stats, bookmark
+    
+    // Can delete recipe
+}
